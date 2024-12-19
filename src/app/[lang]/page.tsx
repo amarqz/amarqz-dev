3a68c32e07e81ca8fc873a5678138349060bfc45
amarqz <git@amarqzp.com>
2024-12-19 09:57:12 +0100

    FEAT: Add Footer

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import type { Metadata, ResolvingMetadata } from "next";
 import { getDictionary } from "./dictionaries";
 import Section from "@/components/Section";
+import Footer from "@/components/Footer";
 
 type Props = {
   params: Promise<{ lang: string }>,
@@ -22,11 +23,14 @@ export default async function Home(props: Props) {
   const sections = ['experience', 'education', 'projects'];
 
   return (
-    <div className="flex flex-col px-8 sm:px-12 gap-12">
-      { sections.map( (section, i) => 
-          <Section key={ i } title={ dict.section[section] } section={ section } locale={ params.lang } />
-        )
-      }
-    </div>
+    <>
+      <div className="flex flex-col px-8 sm:px-12 gap-12">
+        { sections.map( (section, i) => 
+            <Section key={ i } title={ dict.section[section] } section={ section } locale={ params.lang } />
+          )
+        }
+      </div>
+      <Footer />
+    </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  const date = new Date();
+
+  return (
+    <div className="mt-16 py-5 w-screen bg-neutral flex flex-col items-center">
+      <span className="text-accent font-bold">by amarqz.dev</span>
+      <p className="text-accent">2023-{ date.getFullYear() }</p>
+    </div>
+  );
+};
+
+export default Footer;
